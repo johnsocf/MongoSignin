ce31f92d9422c22e54c061cd631522d6cef43302
Catherine Johnson <cfjohnson@earthlink.net>
2014-07-04 20:57:19 -0600

    built out login verification w. mongodb.

--- node/mongo-authentication/server.js
+++ node/mongo-authentication/server.js
@@ -22,6 +22,10 @@ app.get('/', function (req, res) {
 app.get('/login/:signupEmail', function (req, res) {
   res.render('login', { signupEmail: req.params.signupEmail });
 });
+app.get('/logout', function (req, res) {
+	req.esssion.loggedIn = null;
+	res.redirect('/');
+});
 
 app.get('/signup', function (req, res) {
 	res.render('signup');
@@ -33,27 +37,48 @@ new mongodb.Db('my-website', server).open(function (err, client) {
 	console.log('\033[96m + \033[39m connected to mongodb');
 	app.users = new mongodb.Collection(client, 'users');
 
+	client.ensureIndex('users', 'password', function (err) {
+		if (err) throw err;
+
+		console.log('\033[96m + \033[39m ensured indexes');
+	});
+
 });
 
 app.post('/signup', function (req, res, next) {
 	app.users.insert(req.body.user, function (err, records) {
 		if (err) return next(err);
-		
 	});
 	setTimeout(function() {
 
     // Fetch the document
     app.users.findOne(req.body.user, function(err, item) {
       if (err) return next(err);
-      console.log(item.email);
+      console.log(req.body);
 
       res.redirect('/login/' + item.email);
-      server.close();
+      //server.close();
     })
   }, 100);
 });
 
 
+app.post('/login', function (req, res, next) {
+	setTimeout(function() {
+
+    // Fetch the document
+    app.users.findOne({email: req.body.user.email, password: req.body.user.password}, function(err, item) {
+      if (err) return next(err);
+      if (!item) console.log('<p>User not found. Go back and try again');
+      if (item) {
+      	req.session.loggedIn = item._id;
+      	res.redirect('/');
+      	console.log(req.session.loggedIn);
+      }
+      
+    })
+  }, 100);
+});
 
 
 app.listen(3000, function () {
