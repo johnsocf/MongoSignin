49361fd6f46cabdf0342c7a249b49c383de90c31
Catherine Johnson <cfjohnson@earthlink.net>
2014-07-04 22:17:33 -0600

    built out user authenticaiton.

--- node/mongo-authentication/server.js
+++ node/mongo-authentication/server.js
@@ -15,34 +15,32 @@ app.use(session({secret: 'my secret',
 
 app.set('view engine', 'jade');
 
-app.get('/', function (req, res) {
-	res.render('index', { authenticated: false });
+app.use(function (req, res, next) {
+	if (req.session.loggedIn) {
+		res.locals.authenticated = true;
+		console.log(req.session.loggedIn);
+		app.users.findOne({ _id: mongodb.ObjectID.createFromHexString(req.session.loggedIn) }, function (err, item) {
+			if (err) return next(err);
+			if (item) {
+				var myName = item.first;
+				console.log(myName);
+				res.locals.me = myName;
+				next();
+			}
+			console.log(item);
+		});
+	} else {
+		 res.locals.authenticated = false;
+		 next();
+	}
 });
 
-app.get('/login/:signupEmail', function (req, res) {
-  res.render('login', { signupEmail: req.params.signupEmail });
-});
-app.get('/logout', function (req, res) {
-	req.esssion.loggedIn = null;
-	res.redirect('/');
+app.get('/', function (req, res) {
+	res.render('index');
 });
 
 app.get('/signup', function (req, res) {
 	res.render('signup');
-});
-
-var server = new mongodb.Server('127.0.0.1', 27017)
-new mongodb.Db('my-website', server).open(function (err, client) {
-	if (err) throw err;
-	console.log('\033[96m + \033[39m connected to mongodb');
-	app.users = new mongodb.Collection(client, 'users');
-
-	client.ensureIndex('users', 'password', function (err) {
-		if (err) throw err;
-
-		console.log('\033[96m + \033[39m ensured indexes');
-	});
-
 });
 
 app.post('/signup', function (req, res, next) {
@@ -62,6 +60,9 @@ app.post('/signup', function (req, res, next) {
   }, 100);
 });
 
+app.get('/login/:signupEmail', function (req, res) {
+  res.render('login', { signupEmail: req.params.signupEmail });
+});
 
 app.post('/login', function (req, res, next) {
 	setTimeout(function() {
@@ -69,9 +70,9 @@ app.post('/login', function (req, res, next) {
     // Fetch the document
     app.users.findOne({email: req.body.user.email, password: req.body.user.password}, function(err, item) {
       if (err) return next(err);
-      if (!item) console.log('<p>User not found. Go back and try again');
-      if (item) {
-      	req.session.loggedIn = item._id;
+      if (!item) { console.log('<p>User not found. Go back and try again'); }
+      else {
+      	req.session.loggedIn = item._id.toString();;
       	res.redirect('/');
       	console.log(req.session.loggedIn);
       }
@@ -79,6 +80,30 @@ app.post('/login', function (req, res, next) {
     })
   }, 100);
 });
+
+app.get('/logout', function (req, res) {
+	req.session.loggedIn = false;
+	res.redirect('/');
+});
+
+var server = new mongodb.Server('127.0.0.1', 27017)
+new mongodb.Db('my-website', server).open(function (err, client) {
+	if (err) throw err;
+	console.log('\033[96m + \033[39m connected to mongodb');
+	app.users = new mongodb.Collection(client, 'users');
+
+	client.ensureIndex('users', 'password', function (err) {
+		if (err) throw err;
+
+		console.log('\033[96m + \033[39m ensured indexes');
+	});
+
+});
+
+
+
+
+
 
 
 app.listen(3000, function () {
